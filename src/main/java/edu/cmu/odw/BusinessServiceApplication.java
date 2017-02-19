d4246d90d3fc60eba03167510209d4863e9961c3
ubuntu <ubuntu@ip-172-31-62-165.ec2.internal>
2017-02-19 23:35:28 +0000

    Initial commit

---
+++ src/main/java/edu/cmu/odw/BusinessServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.cmu.odw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessServiceApplication.class, args);
+	}
+}
